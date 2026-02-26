22ec2eed34a74ef9081c7a5a677e674d0f2b4071
petaoxley <pgo13@miami.edu>
2026-02-26 22:38:50 +0000

    bjhb

--- js/debug.js
+++ js/debug.js
@@ -1,3 +1,8 @@
 "use strict";
+
 let message = "Hello world!";
 console.log(message);
+alert (message);
+
+document.querySelector("#heading").innerText = message;
+document.getElementById("heading").innerText = message;
