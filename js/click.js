43a0656b60309e4041f435b4b36a3b61039a121e
petaoxley <pgo13@miami.edu>
2026-02-26 23:01:59 +0000

    header

--- js/click.js
+++ js/click.js
@@ -4,4 +4,16 @@ let redClickBox = document.getElementById ("redClick");
 
 
 redClickBox.classList.add("clickBox");
-redClickBox.addEventListener("click",function(){});
+redClickBox.addEventListener("click",function(){
+    //action i want to do when i click on the red box
+    document.querySelector("body").style.backgroundColor = "blueviolet";
+    redClickBox.style.backgroundColor = "orange"
+
+});
+redClickBox.addEventListener("mouseover", function () {
+    redClickBox.style.backgroundColor = "rgba(0,128,0,0.75)"
+}
+
+redClickBox.addEventListener("mouseout", function () {
+    redClickBox.style.backgroundColor = "red"
+}
