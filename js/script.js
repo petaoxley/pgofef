d069b7282a2dd7fcf5b514e97abfa0fb760b0975
petaoxley <pgo13@miami.edu>
2026-03-25 17:26:01 +0000

    debug js change

--- js/script.js
+++ js/script.js
@@ -53,7 +53,6 @@ let message = "It's 3pm Somewhere!";
 console.log(message);
 alert(message);
 
-document.querySelector("#heading").innerText = message;
 document.getElementById("heading").innerText = message;
 
 
